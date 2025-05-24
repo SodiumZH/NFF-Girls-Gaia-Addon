0c84a9e3268c1569d13931386c8e494845b7a276
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2025-05-24 18:30:08 +0800

    repath bauble NO CHERRY

--- src/main/java/net/sodiumzh/nff/girls/gaia/subsystem/bauble/NFFGirlsGaiaBaubles.java
+++ src/main/java/net/sodiumzh/nff/girls/gaia/subsystem/bauble/NFFGirlsGaiaBaubles.java
@@ -1,12 +1,13 @@
 package net.sodiumzh.nff.girls.gaia.subsystem.bauble;
 
+import net.minecraftforge.eventbus.EventBus;
 import net.sodiumzh.nff.girls.gaia.NFFGirlsGaia;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.sodiumzh.nff.girls.gaia.entity.gaia.*;
-import net.sodiumzh.nff.girls.subsystem.baublesystem.NFFGirlsBaubleRegistrations;
-import net.sodiumzh.nff.services.subsystem.baublesystem.RegisterBaubleEquippableMobsEvent;
+import net.sodiumzh.nff.girls.item.bauble.NFFGirlsBaubleRegistrations;
+import net.sodiumzh.nfu.item.bauble.RegisterBaubleEquippableMobsEvent;
 
 @EventBusSubscriber(modid = NFFGirlsGaia.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NFFGirlsGaiaBaubles
