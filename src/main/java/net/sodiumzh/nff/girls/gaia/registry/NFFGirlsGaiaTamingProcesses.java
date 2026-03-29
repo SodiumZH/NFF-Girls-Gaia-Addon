f971283935f4606cb9e7788be89dabe8be905c58
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-03-29 18:05:59 +0800

    Resolve

--- src/main/java/net/sodiumzh/nff/girls/gaia/registry/NFFGirlsGaiaTamingProcesses.java
+++ src/main/java/net/sodiumzh/nff/girls/gaia/registry/NFFGirlsGaiaTamingProcesses.java
@@ -1,6 +1,7 @@
 package net.sodiumzh.nff.girls.gaia.registry;
 
 import net.sodiumzh.nff.girls.entity.tamingprocess.hmag.HmagAlrauneTamingProcess;
+import net.sodiumzh.nff.girls.entity.tamingprocess.hmag.HmagEnderExecutorTamingProcess;
 import net.sodiumzh.nff.girls.gaia.NFFGirlsGaia;
 import net.sodiumzh.nff.girls.gaia.entity.tamingprocess.*;
 import net.sodiumzh.nff.girls.registry.NFFGirlsFriendingItems;
@@ -32,6 +33,8 @@ public class NFFGirlsGaiaTamingProcesses {
     public static final NFURegistry.Accessor<NFFTamingProcess> GAIA_WITCH = TAMING_PROCESSES.register(
         "gaia_witch", () -> new GaiaWitchProcess().setItemGivingTableOverride(NFFGirlsGaiaFriendingItems.WITCH));
 
+    public static final NFURegistry.Accessor<NFFTamingProcess> GAIA_ENDERMAN_B = TAMING_PROCESSES.register(
+        "gaia_enderman_b", () -> new HmagEnderExecutorTamingProcess().setItemGivingTableOverride(NFFGirlsGaiaFriendingItems.ENDERMAN_B));
 
     public static final NFURegistry.Accessor<NFFTamingProcess> GAIA_ANT_WORKER = TAMING_PROCESSES.register(
         "gaia_ant_worker", () -> new GaiaAntWorkerFriendingProcess().setItemGivingTableOverride(NFFGirlsGaiaFriendingItems.ARTHROPOD));
