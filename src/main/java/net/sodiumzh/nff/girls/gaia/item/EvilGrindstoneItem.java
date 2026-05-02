93fc6fcebf68f065b2178849882a657176ec2ef9
SodiumZH <46860829+SodiumZH@users.noreply.github.com>
2026-05-02 13:16:06 +0800

    Update EvilGrindstoneItem.java
    Removing rare skin

--- src/main/java/net/sodiumzh/nff/girls/gaia/item/EvilGrindstoneItem.java
+++ src/main/java/net/sodiumzh/nff/girls/gaia/item/EvilGrindstoneItem.java
@@ -2,6 +2,14 @@ package net.sodiumzh.nff.girls.gaia.item;
 
 import net.minecraft.world.item.ItemStack;
 import net.sodiumzh.nff.girls.gaia.registry.NFFGirlsGaiaItems;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.sodiumzh.nff.girls.entity.INFFGirlsTamed;
+import net.sodiumzh.nff.girls.gaia.entity.IHasRareVariant;
 import net.sodiumzh.nfu.item.NFUItem;
 
 public class EvilGrindstoneItem extends NFUItem {
@@ -18,5 +26,17 @@ public class EvilGrindstoneItem extends NFUItem {
     @Override
     public boolean hasCraftingRemainingItem(ItemStack stack) {
         return true;
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(Player player, LivingEntity target, InteractionHand hand) {
+        if (target instanceof IHasRareVariant hrv
+            && INFFGirlsTamed.get(target).filter(t -> player.equals(t.getOwnerInDimension())).isPresent()
+            && hrv.isRareVariant())
+        {
+            hrv.setRareVariantID(-1);
+            return InteractionResult.sidedSuccess(player.level.isClientSide());
+        }
+        return InteractionResult.PASS;
     }
 }
